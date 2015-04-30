5eb56fd187a85258e3cd5d627428d8aa6c43322f
huanglan513 <huanglan.IT@vdp17-clnt.centaline.com.cn>
2015-04-30 14:20:44 +0800

    BaseLine

---
+++ ExportTableStructure/Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace ExportTableStructure
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnExport = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblLogin = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbxUserName = new System.Windows.Forms.TextBox();
+            this.tbxPassword = new System.Windows.Forms.TextBox();
+            this.lblDBName = new System.Windows.Forms.Label();
+            this.cboDBName = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tbxServerName = new System.Windows.Forms.TextBox();
+            this.btnConnectTest = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.btnSelectedFolder = new System.Windows.Forms.Button();
+            this.tbxSelectedFolder = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnExport
+            // 
+            this.btnExport.Location = new System.Drawing.Point(16, 324);
+            this.btnExport.Name = "btnExport";
+            this.btnExport.Size = new System.Drawing.Size(116, 23);
+            this.btnExport.TabIndex = 8;
+            this.btnExport.Text = "导出表结构";
+            this.btnExport.UseVisualStyleBackColor = true;
+            this.btnExport.Visible = false;
+            this.btnExport.Click += new System.EventHandler(this.btnExport_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(282, 150);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // lblLogin
+            // 
+            this.lblLogin.AutoSize = true;
+            this.lblLogin.Location = new System.Drawing.Point(11, 9);
+            this.lblLogin.Name = "lblLogin";
+            this.lblLogin.Size = new System.Drawing.Size(77, 12);
+            this.lblLogin.TabIndex = 5;
+            this.lblLogin.Text = "登陆到服务器";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(29, 75);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 12);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "用户名";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(29, 97);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 12);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "密码";
+            // 
+            // tbxUserName
+            // 
+            this.tbxUserName.Location = new System.Drawing.Point(77, 66);
+            this.tbxUserName.Name = "tbxUserName";
+            this.tbxUserName.Size = new System.Drawing.Size(161, 21);
+            this.tbxUserName.TabIndex = 2;
+            this.tbxUserName.Text = "NJCCAIUser";
+            // 
+            // tbxPassword
+            // 
+            this.tbxPassword.Location = new System.Drawing.Point(77, 93);
+            this.tbxPassword.Name = "tbxPassword";
+            this.tbxPassword.Size = new System.Drawing.Size(161, 21);
+            this.tbxPassword.TabIndex = 3;
+            this.tbxPassword.Text = "njccaidbuser";
+            this.tbxPassword.UseSystemPasswordChar = true;
+            // 
+            // lblDBName
+            // 
+            this.lblDBName.AutoSize = true;
+            this.lblDBName.Location = new System.Drawing.Point(14, 164);
+            this.lblDBName.Name = "lblDBName";
+            this.lblDBName.Size = new System.Drawing.Size(101, 12);
+            this.lblDBName.TabIndex = 10;
+            this.lblDBName.Text = "选择一个数据库名";
+            // 
+            // cboDBName
+            // 
+            this.cboDBName.FormattingEnabled = true;
+            this.cboDBName.Location = new System.Drawing.Point(12, 190);
+            this.cboDBName.Name = "cboDBName";
+            this.cboDBName.Size = new System.Drawing.Size(258, 20);
+            this.cboDBName.TabIndex = 5;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(14, 47);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(65, 12);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "服务器名称";
+            // 
+            // tbxServerName
+            // 
+            this.tbxServerName.Location = new System.Drawing.Point(77, 38);
+            this.tbxServerName.Name = "tbxServerName";
+            this.tbxServerName.Size = new System.Drawing.Size(161, 21);
+            this.tbxServerName.TabIndex = 1;
+            this.tbxServerName.Text = "10.9.0.26";
+            // 
+            // btnConnectTest
+            // 
+            this.btnConnectTest.Location = new System.Drawing.Point(16, 120);
+            this.btnConnectTest.Name = "btnConnectTest";
+            this.btnConnectTest.Size = new System.Drawing.Size(75, 23);
+            this.btnConnectTest.TabIndex = 4;
+            this.btnConnectTest.Text = "测试服务器";
+            this.btnConnectTest.UseVisualStyleBackColor = true;
+            this.btnConnectTest.Click += new System.EventHandler(this.btnConnectTest_Click);
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.Description = "请选择导出至文件夹";
+            this.folderBrowserDialog1.RootFolder = System.Environment.SpecialFolder.MyComputer;
+            this.folderBrowserDialog1.SelectedPath = "D:";
+            // 
+            // btnSelectedFolder
+            // 
+            this.btnSelectedFolder.Location = new System.Drawing.Point(12, 232);
+            this.btnSelectedFolder.Name = "btnSelectedFolder";
+            this.btnSelectedFolder.Size = new System.Drawing.Size(111, 23);
+            this.btnSelectedFolder.TabIndex = 6;
+            this.btnSelectedFolder.Text = "选择导出至文件夹";
+            this.btnSelectedFolder.UseVisualStyleBackColor = true;
+            this.btnSelectedFolder.Click += new System.EventHandler(this.btnSelectedFolder_Click);
+            // 
+            // tbxSelectedFolder
+            // 
+            this.tbxSelectedFolder.Location = new System.Drawing.Point(12, 261);
+            this.tbxSelectedFolder.Name = "tbxSelectedFolder";
+            this.tbxSelectedFolder.Size = new System.Drawing.Size(256, 21);
+            this.tbxSelectedFolder.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 390);
+            this.Controls.Add(this.tbxSelectedFolder);
+            this.Controls.Add(this.btnSelectedFolder);
+            this.Controls.Add(this.btnConnectTest);
+            this.Controls.Add(this.tbxServerName);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.cboDBName);
+            this.Controls.Add(this.lblDBName);
+            this.Controls.Add(this.tbxPassword);
+            this.Controls.Add(this.tbxUserName);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblLogin);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnExport);
+            this.Name = "Form1";
+            this.Text = "导出表结构到文件夹";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnExport;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblLogin;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbxUserName;
+        private System.Windows.Forms.TextBox tbxPassword;
+        private System.Windows.Forms.Label lblDBName;
+        private System.Windows.Forms.ComboBox cboDBName;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox tbxServerName;
+        private System.Windows.Forms.Button btnConnectTest;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button btnSelectedFolder;
+        private System.Windows.Forms.TextBox tbxSelectedFolder;
+    }
+}
+
---
+++ ExportTableStructure/Form1.cs
@@ -0,0 +1,290 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Microsoft.Office.Interop.Excel;
+using System.IO;
+
+namespace ExportTableStructure
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExport_Click(object sender, EventArgs e)
+        {
+            if (tbxSelectedFolder.Text.Trim() == "")
+            {
+                MessageBox.Show("请选择文件夹", "输入提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                tbxSelectedFolder.Focus();
+            }
+            if(!Directory.Exists(tbxSelectedFolder.Text.Trim()))
+            {
+                MessageBox.Show("此文件夹不存在，请重新选择有效的文件夹", "输入提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                tbxSelectedFolder.Focus();
+            }
+            btnExport.Text = "请等待......";
+
+            string filePath = string.Empty;
+            if (tbxSelectedFolder.Text.Trim().LastIndexOf("\\") == tbxSelectedFolder.Text.Trim().Length-1)
+                filePath = tbxSelectedFolder.Text.Trim();
+            else
+                filePath = tbxSelectedFolder.Text.Trim() + "\\";
+            filePath += cboDBName.Text + "DBStructure" + DateTime.Now.ToString("yyyyMMddHHmmss") + ".xls";
+            if (!File.Exists(filePath))
+            {
+                File.Create(filePath).Close(); 
+            }
+
+            InsertIntoExcel(filePath);
+            btnExport.Text = "导出表结构";
+        }
+
+        private void InsertIntoExcel(string filePath)
+        {
+            //    string filePath = "D:\\SCCCAIDBStructure20121120152025.xls";
+            //   string strConn = "server=10.12.1.96;uid=SCCCAIUser;pwd=scccaidbuser;database=SCCCAI";
+            // SqlHelper sqlHelper = new SqlHelper(strConn);
+
+
+            SqlHelper sqlHelper = new SqlHelper(tbxServerName.Text.Trim(), cboDBName.Text, tbxUserName.Text.Trim(), tbxPassword.Text.Trim());
+
+            //第一条Sql查出所有的表名，第二条Sql查出所有表下的列的属性
+            string sql = @"select name as TableName,'' as [Desc] from dbo.SysObjects where xtype='U' order by name;
+                           SELECT obj.name as  N'表名', col.colorder as N'字段序号', col.name as  N'字段名', 
+                            (case when COLUMNPROPERTY( col.id,col.name,'IsIdentity')=1 then '√'else '' end) as  N'标识', 
+                            (case when (SELECT count(*) 
+                                        FROM sysobjects WHERE (name in 
+                                                               (SELECT name 
+                                                                FROM sysindexes 
+                                                                WHERE (id = col.id) AND (indid in 
+                                                                     (SELECT indid 
+                                                                        FROM sysindexkeys 
+                                                                        WHERE (id = col.id) AND (colid in 
+                                                                              (SELECT colid  FROM syscolumns 
+                                                                                 WHERE (id = col.id) AND (name = col.name))))))) 
+                                                                                 AND 
+                            (xtype = 'PK'))>0 then '√' else '' end) as N'主键', 
+                            type.name as N'类型', 
+                            col.length N'占用字节数', 
+                            COLUMNPROPERTY(col.id,col.name,'PRECISION') as N'字段定义的长度', 
+                            isnull(COLUMNPROPERTY(col.id,col.name,'Scale'),0) as N'小数点后位数', 
+                            (case when col.isnullable=1 then '√'else '' end) as N'是否允许空', 
+                            isnull(e.text,'') as N'默认值',
+                            isnull(g.[value],'') as N'字段说明' 
+
+                            FROM syscolumns col left join systypes type
+                            on col.xtype=type.xusertype 
+                            inner join sysobjects obj 
+                            on col.id=obj.id and obj.xtype='U' and obj.name<>'dtproperties' 
+                            left join syscomments e 
+                            on col.cdefault=e.id 
+                            left join sys.extended_properties   g 
+                            on col.id=g.major_id AND col.colid = g.minor_id 
+                            order by object_name(col.id),col.colorder ";
+            DataSet ds = sqlHelper.ExecuteSqlDataSet(sql);
+
+
+            Microsoft.Office.Interop.Excel.Application app = new ApplicationClass();
+            try
+            {
+                #region
+                System.Data.DataTable excelTable = null;
+                app.Visible = false;
+                Workbook wBook = app.Workbooks.Add(true);
+                Worksheet wSheet = null;
+                string columnSpan = string.Empty;
+                for (int k = 0; k < ds.Tables.Count; k++)
+                {
+                    if (wBook.Worksheets.Count < k + 1)
+                    {
+                        wSheet = wBook.Worksheets.Add(Type.Missing, wBook.Worksheets[wBook.Worksheets.Count], Type.Missing, Type.Missing) as Worksheet;
+                    }
+                    else
+                    {
+                        wSheet = wBook.Worksheets[k + 1] as Worksheet;
+                    }
+                    wSheet.Cells.Clear();
+
+                    if (k == 0)
+                    {
+                        wSheet.Name = "Tables Name";
+                        columnSpan = "B";
+                    }
+                    else
+                    {
+                        wSheet.Name = "Columns Name";
+                        columnSpan = "L";
+                    }
+
+                    excelTable = ds.Tables[k];
+                    if (excelTable.Rows.Count > 0)
+                    {
+                        int row = excelTable.Rows.Count;
+                        int col = excelTable.Columns.Count;
+
+                        Range range = SetRangeProperty(wSheet, row, columnSpan);
+
+                        if (k == 0)
+                        {
+                            for (int i = 0; i < row; i++)
+                            {
+                                for (int j = 0; j < col - 1; j++)
+                                {
+                                    string str = excelTable.Rows[i][j].ToString();
+                                    wSheet.Cells[i + 2, j + 1] = str;
+                                }
+                            }
+                        }
+                        else
+                        {
+                            string preTableName = excelTable.Rows[0][0].ToString();
+                            for (int i = 0; i < row; i++)
+                            {
+                                if (preTableName != excelTable.Rows[i][0].ToString())
+                                {
+                                    wSheet.Range[wSheet.Cells[i + 2, 1], wSheet.Cells[i + 2, col]].Borders[XlBordersIndex.xlEdgeTop].LineStyle = XlLineStyle.xlContinuous;
+                                    wSheet.Range[wSheet.Cells[i + 2, 1], wSheet.Cells[i + 2, col]].Borders[XlBordersIndex.xlEdgeTop].Weight = XlBorderWeight.xlMedium;
+                                    preTableName = excelTable.Rows[i][0].ToString();
+                                }
+                                for (int j = 0; j < col; j++)
+                                {
+                                    string str = excelTable.Rows[i][j].ToString();
+                                    wSheet.Cells[i + 2, j + 1] = str;
+                                }
+                            }
+                        }
+                    }
+
+                    int size = excelTable.Columns.Count;
+                    for (int i = 0; i < size; i++)
+                    {
+                        wSheet.Cells[1, 1 + i] = excelTable.Columns[i].ColumnName;
+
+                    }
+                }
+                //设置禁止弹出保存和覆盖的询问提示框   
+                app.DisplayAlerts = false;
+                app.AlertBeforeOverwriting = false;
+                //保存工作簿   
+               // wBook.Save();
+                  wBook.SaveCopyAs(filePath);
+                //保存excel文件   
+               // app.Save(filePath);
+               // app.SaveWorkspace(filePath);
+                app.Quit();
+                app = null;
+                #endregion
+                MessageBox.Show("导出成功", "提示信息",
+                   MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show("导出Excel出错！错误原因：" + err.Message, "提示信息",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private static Range SetRangeProperty(Worksheet wSheet, int row,string columnSpan)
+        {
+            Range range = wSheet.get_Range("A1", columnSpan + (row + 1).ToString());
+            range.Font.Size = 10;     //设置字体大小
+            range.Font.Name = "黑体";     //设置字体的种类
+            range.ColumnWidth = 33;     //设置单元格的宽度
+            //   range.EntireColumn.AutoFit();     //自动调整列宽
+            range.Borders.LineStyle = 1;     //设置单元格边框的粗细
+           
+            range.BorderAround(XlLineStyle.xlContinuous, XlBorderWeight.xlMedium, XlColorIndex.xlColorIndexAutomatic, System.Drawing.Color.Black.ToArgb());     //给单元格加边框
+
+            Range rangeHeader = wSheet.get_Range("A1", columnSpan+"1");
+            rangeHeader.HorizontalAlignment = XlHAlign.xlHAlignCenter;
+            rangeHeader.Font.Bold = true;
+            rangeHeader.Cells.Interior.Color = System.Drawing.Color.FromArgb(255, 204, 153).ToArgb();     //设置单元格的背景色
+            return range;
+        }
+
+        //private void btnSearchServer_Click(object sender, EventArgs e)
+        //{
+        //    cboServers.Items.Clear();
+        //    SQLDMO.ApplicationClass sqlApp = new SQLDMO.ApplicationClass();
+        //    SQLDMO.NameList namelist;
+        //    namelist = sqlApp.ListAvailableSQLServers();
+        //    for(int i=0;i<namelist.Count;i++)
+        //    {
+        //        object obj = namelist.Item(i);
+        //        if (obj != null)
+        //        {
+        //            this.cboServers.Items.Add(obj);
+        //        }
+        //    }
+        //    if (this.cboServers.Items.Count > 0)
+        //        this.cboServers.SelectedIndex = 0;
+        //    else
+        //        this.cboServers.Text = "<No available SQL Servers>";
+        //}
+
+        private void btnConnectTest_Click(object sender, EventArgs e)
+        {
+            if (tbxServerName.Text.Trim() == "")
+            {
+                MessageBox.Show("请输入服务器名称", "输入提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                tbxServerName.Focus();
+            }
+            if (tbxUserName.Text.Trim() == "")
+            {
+                MessageBox.Show("请输入用户名", "输入提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                tbxUserName.Focus();
+            }
+            if (tbxPassword.Text.Trim() == "")
+            {
+                MessageBox.Show("请输入密码", "输入提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                tbxPassword.Focus();
+            }
+
+            cboDBName.Items.Clear();
+            SQLDMO.SQLServer srv = new SQLDMO.SQLServerClass();
+            try
+            {
+                srv.Connect(tbxServerName.Text.Trim(),tbxUserName.Text.Trim(),tbxPassword.Text.Trim());
+                foreach(SQLDMO.Database db in srv.Databases)
+                {
+                    if(db.Name!=null)
+                    {
+                        cboDBName.Items.Add(db.Name);
+                    }
+                }
+                if(this.cboDBName.Items.Count!=0)
+                    cboDBName.SelectedIndex=0;
+                MessageBox.Show("服务器连接成功！","提示",MessageBoxButtons.OK,MessageBoxIcon.Information);
+                btnExport.Visible = true;
+            }
+            catch
+            {
+                MessageBox.Show("连接服务器：" + this.tbxServerName.Text.Trim() + "失败！\n请核对用户名和密码。", "提示：", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnSelectedFolder_Click(object sender, EventArgs e)
+        {
+            if (this.folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                if (this.folderBrowserDialog1.SelectedPath.Trim() != "")
+                    this.tbxSelectedFolder.Text = this.folderBrowserDialog1.SelectedPath.Trim();
+            }
+        }
+
+     
+
+       
+
+      
+    }
+}
